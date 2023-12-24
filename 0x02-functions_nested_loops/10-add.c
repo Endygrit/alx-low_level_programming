4260cf644bb9811c57691a5beed53bb421c2bd4c
Endurance Ine Edward <edwardendurance94@gmail.com>
2023-12-24 07:40:20 -0800

    The A ddition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: Parameter 1
+ * @b: Parameter 2
+ *
+ * Return: the result
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
