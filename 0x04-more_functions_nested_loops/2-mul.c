b28b95f833840ecb795d101730f12b45a4a46bb7
Endurance Ine Edward <edwardendurance94@gmail.com>
2023-12-29 00:04:07 -0800

    Multiple Collaboration

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
