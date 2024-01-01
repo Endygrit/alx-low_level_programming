0982e5fdac083d29ffa53cf443042a508e0c368b
Endurance Ine Edward <edwardendurance94@gmail.com>
2024-01-01 03:06:20 -0800

    Unknown Integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,20 +4,21 @@
  * print_number - prints an integer
  * @n: the parameter that holds the integer
  *
- * Return: the result
+ * Return: void
  */
 
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
+		n *= -1;
+		i = n;
 		_putchar('-');
-		n = -n;
 	}
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
-	_putchar('\n');
+	i /= 10;
+	if (i != 0)
+		print_number(i);
+	_putchar((unsigned int) n % 10 + '0');
 }
